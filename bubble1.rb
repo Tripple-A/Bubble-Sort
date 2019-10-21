368a571552dc046276336db4dc5eea0056be6552
Biodun <biodun9@gmail.com>
2019-10-21 17:16:03 +0100

    added code for bubble_sort

---
+++ bubble1.rb
@@ -0,0 +1,10 @@
+def bubble_sort(arr)
+  j = arr.length - 1
+  while j.positive?
+    (0...j).each do |i|
+      arr[i], arr[i + 1] = arr[i + 1], arr[i] if arr[i] > arr[i + 1]
+    end
+    j -= 1
+  end
+  print arr
+end
